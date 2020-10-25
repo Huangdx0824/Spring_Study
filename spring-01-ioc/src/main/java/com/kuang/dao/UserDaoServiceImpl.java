13dcceaceaeacc05e676be07f960dc4f6022a5b6
zero <1580359334@qq.com>
2020-10-25 08:06:04 +0800

    首次提交

---
+++ spring-01-ioc/src/main/java/com/kuang/dao/UserDaoServiceImpl.java
@@ -0,0 +1,7 @@
+package com.kuang.dao;
+
+public class UserDaoServiceImpl implements UserDao {
+    public void getUser() {
+        System.out.println("获取Service信息");
+    }
+}
---
+++ spring-02-hellospring/src/main/java/com/huang/pojo/Hello.java
@@ -0,0 +1,21 @@
+package com.huang.pojo;
+
+public class Hello {
+    //这是一个实体类
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return "Hello{" +
+                "str='" + str + '\'' +
+                '}';
+    }
+}
